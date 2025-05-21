85108b2a91b3f8a759e5d0057c1803fdd020b4a5
Joshua Henninger <joshua@triumph.tech>
2025-05-21 15:49:50 -0700

    - WIP: Progress toward Communication Flow Detail wizard.

---
+++ Rock.Blocks/Communication/CommunicationFlowPerformance.cs
@@ -0,0 +1,89 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+
+using System.Collections.Generic;
+using System.ComponentModel;
+
+using Rock.Attribute;
+using Rock.Model;
+using Rock.ViewModels.Blocks;
+
+namespace Rock.Blocks.Communication
+{
+    /// <summary>
+    /// Displays the performance of a particular communication flow.
+    /// </summary>
+
+    [DisplayName( "Communication Flow Performance" )]
+    [Category( "Communication" )]
+    [Description( "Displays the performance of a particular communication flow." )]
+    [IconCssClass( "fa fa-line-chart" )]
+    // [SupportedSiteTypes( Model.SiteType.Web )]
+
+    #region Block Attributes
+
+    #endregion
+    
+    [Rock.SystemGuid.EntityTypeGuid( "53FFF4C5-1F30-415C-BE65-53FCA7F2CD64" )]
+    [Rock.SystemGuid.BlockTypeGuid( "72B92AA4-2AA7-4FDD-9CD7-7DD84018B21E" )]
+    public class CommunicationFlowPerformance : RockBlockType
+    {
+        #region Keys
+
+        private static class PageParameterKey
+        {
+            public const string CommunicationFlow = "CommunicationFlow";
+        }
+
+        private static class NavigationUrlKey
+        {
+            public const string ParentPage = "ParentPage";
+        }
+
+        #endregion Keys
+
+        #region Methods
+
+        /// <inheritdoc/>
+        public override object GetObsidianBlockInitialization()
+        {
+            var box = new BlockBox();
+
+            box.NavigationUrls = GetBoxNavigationUrls();
+
+            return box;
+        }
+
+        /// <summary>
+        /// Gets the box navigation URLs required for the page to operate.
+        /// </summary>
+        /// <returns>A dictionary of key names and URL values.</returns>
+        private Dictionary<string, string> GetBoxNavigationUrls()
+        {
+            return new Dictionary<string, string>
+            {
+                [NavigationUrlKey.ParentPage] = this.GetParentPageUrl()
+            };
+        }
+
+        #endregion
+
+        #region Block Actions
+
+        #endregion
+    }
+}
